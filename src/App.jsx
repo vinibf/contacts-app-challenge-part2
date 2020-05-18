a31d14254d011c239db2efb00a93e753f9f7039c
Vinicius BF <vini91bf@gmail.com>
2020-05-18 01:38:29 -0300

    Defined the life cycle methods of App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,40 @@ import Topbar from './components/Topbar';
 import Filters from './components/Filters';
 import Contacts from './components/Contacts';
 
+import api from './services/api';
+//import ordering from './services/ordering';
+
 import './App.scss';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchString: "",
+            contactList: [],
+            filteredContacts: [],
+            loadingContacts: false,
+            sortingType: ""
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            loadingContacts: true
+        });
+
+        api.get("contacts").then(data => {
+            console.log(data);
+
+            this.setState({
+                contactList: data,
+                filteredContacts: data,
+                loadingContacts: false
+            });
+            // Ordenar por 'nome' ao carregar
+        });
+    }
+
 	render() {
 		return (
 			<React.Fragment>
